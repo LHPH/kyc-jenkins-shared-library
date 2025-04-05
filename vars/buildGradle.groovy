dfab888289a75a967acef3392477850db8d663b6
LHPH <luislyly@hotmail.com>
2025-04-05 15:48:08 -0600

    Remove full path and leave the command

--- vars/buildGradle.groovy
+++ vars/buildGradle.groovy
@@ -5,5 +5,5 @@ def call(Map config = [:]){
         skipTest = '-x test -x jacocoTestCoverageVerification';
     }
 
-    sh "/opt/gradle/gradle-8.13/bin/gradle clean build ${skipTest} --info --stacktrace".trim();
+    sh "gradle clean build ${skipTest} --info --stacktrace".trim();
 }
