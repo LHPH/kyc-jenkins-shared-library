42edbc90a0c675714ef6ff5ed8718b6a132850bc
LHPH <luislyly@hotmail.com>
2026-07-04 12:09:32 -0600

    Adjust in buildAndroid for renaming file

--- vars/buildAndroid.groovy
+++ vars/buildAndroid.groovy
@@ -1,10 +1,17 @@
 def call(Map config = [:]){
     
-   def buildType = 'Debug' 
+   def flavourVersion = "${config.flavour}Version" 
+   def buildType = 'Debug'
    if(config.branch == 'master'){
       buildType = 'Release'
    }
 
     sh "gradle -v"
-    sh "gradle assemble${config.flavour}Version${buildType} --info --stacktrace".trim();
+    sh "gradle assemble${flavourVersion}${buildType} --info --stacktrace".trim();
+    sh """
+        cd app/build/outputs/apk/${flavourVersion}/${buildType.toLowerCase()}
+        for file in app-*-${buildType.toLowerCase()}; do 
+            mv "$file" "${file%.apk}-${BUILD_VERSION}.apk"
+        done
+    """
 }
