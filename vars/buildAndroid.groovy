70c85ae061324b11caf2495a64aeee376cdf5f2c
LHPH <luislyly@hotmail.com>
2026-07-04 12:29:42 -0600

    Fix script buildAndroid

--- vars/buildAndroid.groovy
+++ vars/buildAndroid.groovy
@@ -9,7 +9,8 @@ def call(Map config = [:]){
     sh "gradle -v"
     sh "gradle assemble${flavourVersion}${buildType} --info --stacktrace".trim();
     sh """
-        cd app/build/outputs/apk/${flavourVersion}/${buildType.toLowerCase()}
+        cd app/build/outputs/apk/${flavourVersion.uncapitalize()}/${buildType.toLowerCase()}
+        ls -lta
         for file in app-*-${buildType.toLowerCase()}.apk; do 
             mv "\$file" "\${file%.apk}.${env.BUILD_NUMBER}.apk"
         done
