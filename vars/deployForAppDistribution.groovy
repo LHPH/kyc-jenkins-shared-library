4eac5893ae293a5e7a61721e20ea1f30ff902a09
LHPH <luislyly@hotmail.com>
2026-07-07 19:28:10 -0600

    Changes to test deployForAppDistribution

--- vars/deployForAppDistribution.groovy
+++ vars/deployForAppDistribution.groovy
@@ -2,15 +2,29 @@ def call(Map config = [:]){
 
     def flavourVersion = "${config.flavour}" 
     def buildType = 'Debug'
+    def format = config.format.uncapitalize();
+
     if(config.branch == 'master'){
-       buildType = 'Release'
+        buildType = 'Release'
     }
 
-    def buildApkPath = "app/build/outputs/apk/${flavourVersion.uncapitalize()}/${buildType.toLowerCase()}/"
+    if(format == 'aab'){
+        buildFormat = 'bundle';
+    }
+
+    def buildPath = "${env.WORKSPACE}/app/build/outputs/${format}/${flavourVersion.uncapitalize()}/${buildType.uncapitalize()}/"
 
     sh """
-        FILE_PATH=\$(find ${buildApkPath} -maxdepth 1 -type f -name "${config.project}-*.apk")
+        FILE_PATH=\$(find ${buildPath} -maxdepth 1 -type f -name "${config.project}-*.${format}")
+        ls -lta \$FILE_PATH
         echo \$FILE_PATH
-        gradle appDistributionUpload${config.flavour}${buildType} -PfirebaseAppDistribution.apkPath="\$FILE_PATH" --info --stacktrace
     """
+
+    /*sh """
+        gradle appDistributionUpload${config.flavour}${buildType} --artifactPath="\$FILE_PATH" --artifactType="${format.toUpperCase()}" --info --stacktrace
+
+        FILE_PATH=\$(find ${buildPath} -maxdepth 1 -type f -name "${config.project}-*.${format}")
+        echo \$FILE_PATH
+        gradle appDistributionUpload${config.flavour}${buildType} -PfirebaseAppDistribution.apkPath="\$FILE_PATH" -PartifactType="${format.toUpperCase()}" --info --stacktrace
+    """*/
 }
