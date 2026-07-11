713915dc8a91cd12424ee1ce1a18c18af6d2a9d3
LHPH <luislyly@hotmail.com>
2026-07-11 13:20:08 -0600

    Adjust in buildAndroid to check error

--- vars/buildAndroid.groovy
+++ vars/buildAndroid.groovy
@@ -27,8 +27,9 @@ def call(Map config = [:]){
 
             FILE_PATH=\$(find ${buildPath} -maxdepth 1 -type f -name "${config.project}-*.${format}")
 
-            echo "'${KEYSTORE_BASE64}' | base64 -d > release-key.jks"
-            ls -lta
+            pwd
+            echo "'${KEYSTORE_BASE64}' | base64 -d > ${env.WORKSPACE}/release-key.jks"
+            ls -lta release-key.jks
 
             \$ANDROID_HOME/build-tools/35.0.0/apksigner sign \
             --ks ${env.WORKSPACE}/release-key.jks \
