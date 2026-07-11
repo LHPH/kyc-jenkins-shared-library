e478a359b1e961e26f36ec9fc1305b3f06e6cb0f
LHPH <luislyly@hotmail.com>
2026-07-11 12:53:38 -0600

    First changes to check apksigner

--- vars/buildAndroid.groovy
+++ vars/buildAndroid.groovy
@@ -19,6 +19,27 @@ def call(Map config = [:]){
     sh """
         gradle -v
         gradle ${buildFormat}${flavourVersion}${buildType} --info --stacktrace
+    """
+
+    if(buildType == 'Release'){
+
+        sh """
+
+            FILE_PATH=\$(find ${buildPath} -maxdepth 1 -type f -name "${config.project}-*.${format}")
+
+            echo '${KEYSTORE_BASE64}' | base64 -d > release-key.jks"
+
+            \$ANDROID_HOME/build-tools/35.0.0/apksigner sign \
+            --ks release-key.jks \
+            --ks-pass pass:"${KEYSTORE_PASSWORD}" \
+            --ks-key-alias "${KEY_ALIAS}" \
+            --key-pass pass:"${KEY_PASSWORD}" \
+            --out \$FILE_PATH \
+            \$FILE_PATH
+        """
+    }
+
+    sh """
         cd ${buildPath}
         ls -lta
     """
